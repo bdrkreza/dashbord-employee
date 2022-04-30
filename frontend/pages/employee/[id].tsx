99395c2ea92ad3e0031ff11fd50949cfa9e2465a
bdrkreza <diureza@gmail.com>
2022-05-01 00:34:23 +0600

    frontend setup

---
+++ frontend/pages/employee/[id].tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EditEmployee() {
+  return <div>editEmployee</div>;
+}
---
+++ frontend/pages/project/[id].tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EditProject() {
+  return <div>[id]</div>;
+}
