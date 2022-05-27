5ec87c4f8651d5d7e0f66ce47b03b8b0d3278a16
bdrkreza <diureza@gmail.com>
2022-05-27 23:47:37 +0600

    updated project page

--- frontend/components/project/addProject.tsx
+++ frontend/components/project/addProject.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { BiAddToQueue } from "react-icons/bi";
+import ProjectAddModel from "./projectAddModel";
 type Props = {};
 
 const AddProject = (props: Props) => {
+  const [showModal, setShowModal] = React.useState(false);
+  const handleShowModal = useCallback(() => {
+    setShowModal(!showModal);
+  }, [showModal]);
+
+  const handleCloseModal = useCallback(() => {
+    setShowModal(false);
+  }, []);
   return (
     <div>
       <div className="mt-10">
@@ -11,6 +20,7 @@ const AddProject = (props: Props) => {
             Recent Project Update
           </h1>
           <button
+            onClick={handleShowModal}
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
@@ -18,6 +28,7 @@ const AddProject = (props: Props) => {
             Add Project
           </button>
         </div>
+        {showModal && <ProjectAddModel onCancel={handleCloseModal} />}
         <hr className="divide-solid border-2 border-red-100 " />
       </div>
     </div>
